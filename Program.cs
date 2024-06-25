f4971f4f5f1cf955ea08bdd5145ce8b21fb8a91c
Carlos Thompson <carlosethompson@gmail.com>
2024-06-25 11:48:36 +0100

    feat: update logic and test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "111111", modelo: "Nokia Mod 1", imei: "1111111-1", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("LinkedIn");
+
+Console.WriteLine("-------------------------------");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "222222", modelo: "Iphone Mod 1", imei: "222222-2", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Reddit");
